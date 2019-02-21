dc815bdfd0cabc4e1e81962a2a70bb43a3c7dd9f
ALKETON <www.stark73@yandex.ru>
2019-02-22 01:50:17 +0300

    initial commit

---
+++ src/test/java/alketon/kadastr/KadastrApplicationTests.java
@@ -0,0 +1,16 @@
+package alketon.kadastr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KadastrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
